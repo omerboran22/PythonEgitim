c9697ecd509b55d59d9bc8a87ca657324ad20c5f
omerboran22 <omerboran22@gmail.com>
2022-06-30 00:22:25 +0300

    eğer yazı gelirse hata mesaji veriliyor

--- AsaySayiBulma/asalSayiBulInput.py
+++ AsaySayiBulma/asalSayiBulInput.py
@@ -1,11 +1,17 @@
 def asalSayiBul():
-  limit =int(input("asal Sayı Sınırını Gir Lütfen : "))
-  for i in range(2,limit):
-    kontrol=0
+ 
+ try:
+    limit =int(input("asal Sayı Sınırını Gir Lütfen : "))
+    for i in range(2,limit):
+      kontrol=0
     for a in range(2,i):
       if (i%a==0):
         kontrol=1
     if (kontrol==0):
       print(i,"Asal Sayıdır")
+ except ValueError:
+   print("lütfen Sayı Giriniz")
+   asalSayiBul()
+ 
 
 asalSayiBul()
